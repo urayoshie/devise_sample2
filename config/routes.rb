7e7a122e01fd743b07a1fe94266725c3ce334f2f
Yoshie <yoshieura@icloud.com>
2021-01-02 20:26:14 +0900

    商品の登録機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  # get 'items/index'
+  # get 'items/new'
+  # get 'items/create'
+  # devise_for :users
+  root to: 'items#index'
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :items
+
+  resource :users, only: :show
 end
